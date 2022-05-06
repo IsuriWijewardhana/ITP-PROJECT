c57814d52b02c5dae9b8653c922210ba3429901c
Navindu Praveen <89752602+Navindu-Praveen@users.noreply.github.com>
2022-05-06 19:12:48 +0530

    add Accordion to myevents

--- EVENT MANAGEMENT SYSTEM/EVENT MANAGEMENT/frontend/src/screens/MyEvents/MyEvents.js
+++ EVENT MANAGEMENT SYSTEM/EVENT MANAGEMENT/frontend/src/screens/MyEvents/MyEvents.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Badge, Button, Card } from "react-bootstrap";
+import { Accordion, Badge, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import MainScreen from "../../components/MainScreen";
 import events from "../../data/events";
@@ -16,45 +16,53 @@ const MyEvents = () => {
           Create New Event
         </Button>
       </Link>
+
       {events.map((event) => (
-        <Card style={{ margin: 10 }}>
-          <Card.Header style={{ display: "flex" }}>
-            <span
-              style={{
-                color: "black",
-                textDescription: "none",
-                flex: 1,
-                cusor: "pointer",
-                alignSelf: "center",
-                fontSize: 18,
-              }}
-            >
-              {event.title}
-            </span>
-
-            <div>
-              <Button href={`/event/${event._id}`}>Edit</Button>
-              <Button
-                variant="danger"
-                className="mx-2"
-                onClick={() => deleteHandler(event._id)}
+        <Accordion>
+          <Card style={{ margin: 10 }}>
+            <Card.Header style={{ display: "flex" }}>
+              <span
+                style={{
+                  color: "black",
+                  textDescription: "none",
+                  flex: 1,
+                  cusor: "pointer",
+                  alignSelf: "center",
+                  fontSize: 18,
+                }}
               >
-                Delete
-              </Button>
-            </div>
-          </Card.Header>
+                <Accordion.Button as={Card.Text} variant="link" eventKey="0">
+                  {event.title}
+                </Accordion.Button>
+              </span>
 
-          <Card.Body>
-            <h4>
-              <Badge bg="success">Category - {event.category}</Badge>
-            </h4>
-            <blockquote className="blockquote mb-0">
-              <p>{event.content}</p>
+              <div>
+                <Button href={`/event/${event._id}`}>Edit</Button>
+                <Button
+                  variant="danger"
+                  className="mx-2"
+                  onClick={() => deleteHandler(event._id)}
+                >
+                  Delete
+                </Button>
+              </div>
+            </Card.Header>
+            <Accordion.Body>
+              <Card.Body>
+                <h4>
+                  <Badge variant="success">Category - {event.category}</Badge>
+                </h4>
+                <blockquote className="blockquote mb-0">
+                  <p>{event.content}</p>
 
-              <footer className="blockquote-footer">Created On - date</footer>
-            </blockquote>
-          </Card.Body>
-        </Card>
+                  <footer className="blockquote-footer">
+                    Created On - date
+                  </footer>
+                </blockquote>
+              </Card.Body>
+            </Accordion.Body>
+          </Card>
+        </Accordion>
       ))}
     </MainScreen>
   );
